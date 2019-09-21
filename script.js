9e365c80b3bc2715a2848e9bd73b73eeb1d721a1
Herve Seka <sekaherve@gmail.com>
2019-09-21 04:44:54 +0200

    JQuery UI DatePicker, avec des paramètres permettant de modifier directement les mois et les années

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ $(function(){
   $("#datepicker").datepicker({
     altField:"#input2",
     altFormat: "yy-mm-dd",
-    appendText: "yyyy-mm-dd"
+    appendText: "yyyy-mm-dd",
+    changeMonth: true,
+    changeYear: true
   });
 });
